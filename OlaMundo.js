6f67da75568820ffa11606b120f1d724fb687f8c
João Victor <48702646+jvac99@users.noreply.github.com>
2021-12-01 13:30:12 -0300

    ola

---
+++ OlaMundo.js
@@ -0,0 +1,6 @@
+console.log("Olá Mundo!!!") // Português
+console.log("Hello world!!!") // Inglês
+console.log("Hola mundo!!!")// espanhol
+console.log("Bonjour le monde!!!") // Francês
+console.log("Ciao mondo!!!") // italiano
+console.log("Hallo Welt!!!") // Alemão
